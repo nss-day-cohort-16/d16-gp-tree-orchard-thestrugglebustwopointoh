d446c11d0b585c6f530f356a61807f3d4a664be4
Shawn M <forgeforza@outlook.com>
2016-11-21 11:49:35 -0600

    template pull

---
+++ javascripts/main.js
@@ -0,0 +1,37 @@
+"use strict";
+
+
+//Plant function should be defined 
+// Use function Arrows? 
+
+Plant.prototype.increaseHeight = function (growth) {
+
+increaseHeight : function () => 
+
+
+}
+
+decreaseHeight : function () =>
+
+
+
+
+//Tree function 
+
+
+
+//increaseHeight
+
+
+//decreaseHeight
+
+
+
+//height
+
+
+
+//grow
+
+
+//trim
